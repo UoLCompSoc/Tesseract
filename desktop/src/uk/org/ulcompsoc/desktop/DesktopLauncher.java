7e268940564d91a9499c3ed3cab7a7f344bc87a3
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-08-23 02:57:08 +0100

    Add debugging mode

--- desktop/src/uk/org/ulcompsoc/desktop/DesktopLauncher.java
+++ desktop/src/uk/org/ulcompsoc/desktop/DesktopLauncher.java
@@ -1,12 +1,20 @@
 package uk.org.ulcompsoc.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import uk.org.ulcompsoc.TesseractMain;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TesseractMain(), config);
+		boolean debug = false;
+
+		for (String s : args) {
+			if ("--debug".equals(s)) {
+				debug = true;
+			}
+		}
+		new LwjglApplication(new TesseractMain(debug), config);
 	}
 }
