e11340e91a58c67b83ed1a06b876abe2fbe75a11
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-08-23 02:50:22 +0100

    Initial commit of barebones project

---
+++ desktop/src/uk/org/ulcompsoc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.org.ulcompsoc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.org.ulcompsoc.TesseractMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TesseractMain(), config);
+	}
+}
